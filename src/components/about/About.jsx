0b5ccb83d630661f0785c67ba3f6967135d092ef
unknown <luisalbertofes@gmail.com>
2023-02-18 15:12:31 -0500

    actualizando

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function About () {
      return <div style={{color:"green", marginTop:"150px"}}>
           <h1>Luis Feste</h1>
-          <img src="../../assets/feste.jpg" alt="" />
+          <img src=".././assets/feste.jpg" alt="" />
           <h6>App de Rick and Morty - M2 -Henry</h6>
 
      </div>
--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -8,7 +8,7 @@ import style from "./Nav.module.css"
 export default function nav({onSearch}){
      return <nav >
           <img src="https://1.bp.blogspot.com/-AvVxtmDEbCw/YRQjDiXFhCI/AAAAAAABAR8/lNQZ_fg-C9UXdoNkqJ3uVOwANlu7S_ZAACLcBGAsYHQ/s3200/tipografia-rick-y-morty.jpg"/>
-          <Link to="/home" className={style.link}>Home</Link>
+          <Link to="/home" className={style.links}>Home</Link>
           <Link to="/about" className={style.links}>About</Link>
           
           <SearchBar onSearch={onSearch}/>
